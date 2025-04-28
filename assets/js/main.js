4014d4201a0c5efd6960a4d819f62ea8a19ec42c
proGamalSherif <jemyxsherifx@gmail.com>
2025-04-28 16:28:00 +0300

    fix path2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
 const imageArr = [{
         name: 'Image 1',
-        path: '/FinalAfaneh/assets/images/Slider1.jpg'
+        path: 'assets/images/Slider1.jpg'
     },
     {
         name: 'Image 2',
-        path: '/FinalAfaneh/assets/images/Slider2.jpg'
+        path: 'assets/images/Slider2.jpg'
     },
     {
         name: 'Image 3',
-        path: '/FinalAfaneh/assets/images/Slider3.jpg'
+        path: 'assets/images/Slider3.jpg'
     },
     {
         name: 'Image 4',
-        path: '/FinalAfaneh/assets/images/Slider4.jpg'
+        path: 'assets/images/Slider4.jpg'
     }
 ];
 let lastIndex = 0;
